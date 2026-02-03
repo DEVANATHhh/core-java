df19cc638c6c71aea9640d5cafef586a0c3c5014
DEVANATHhh <devanath923@gmail.com>
2026-02-04 03:04:52 +0530

    Add files via upload

---
+++ Static Methods/Mobile.java
@@ -0,0 +1,55 @@
+class Mobile {
+
+    static void call() {
+        System.out.println("Calling...");
+    }
+
+    static void message() {
+        System.out.println("Sending message");
+    }
+
+    static void browse() {
+        System.out.println("Browsing internet");
+    }
+
+    static void camera() {
+        System.out.println("Opening camera");
+    }
+
+    static void music() {
+        System.out.println("Playing music");
+    }
+
+    static void video() {
+        System.out.println("Playing video");
+    }
+
+    static void gps() {
+        System.out.println("Using GPS");
+    }
+
+    static void bluetooth() {
+        System.out.println("Bluetooth enabled");
+    }
+
+    static void hotspot() {
+        System.out.println("Hotspot turned on");
+    }
+
+    static void powerOff() {
+        System.out.println("Mobile powered off");
+    }
+
+    public static void main(String[] args) {
+        call();
+        message();
+        browse();
+        camera();
+        music();
+        video();
+        gps();
+        bluetooth();
+        hotspot();
+        powerOff();
+    }
+}
