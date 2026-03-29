e269e9c06b8920770a4a4a38c2744902947c9b88
DEVANATHhh <devanth923@gmail.com>
2026-03-29 22:52:33 +0530

    swapping numbers

---
+++ dsa/SwapNumber.java
@@ -0,0 +1,23 @@
+class SwapNumber{
+
+
+public static void main(String [] args){
+	
+
+int a =10;
+int b =20;
+int temp ;
+
+System.out.println("Before swapping");
+System.out.println("a ="+a+"and b ="+b);
+
+temp=a;
+a=b;
+b=temp;
+
+System.out.println("After swapping");
+System.out.println("a ="+a+"and b ="+b);
+
+}
+
+}
