4ecca69b58dfc20f9407bdf6a3741d4de8eaff34
DEVANATHhh <devanth923@gmail.com>
2026-04-13 00:39:14 +0530

    dsa

---
+++ interview questions/ReverseArray.java
@@ -0,0 +1,10 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+
+        int[] arr={1,6,8,10,16};
+
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.println(arr[i]+"");
+        }
+    }
+}
